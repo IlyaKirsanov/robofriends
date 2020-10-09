fa98d10287457f64c1117bfb0d904b174a324ba8
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-10-09 18:25:08 +0300

    Updates

---
+++ precache-manifest.07ddccbd4d076c8dab65fef0082e36e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f994b2739c5c4e58fa85df7ad2031af2",
+    "url": "/IlyaKirsanov/robofriends/index.html"
+  },
+  {
+    "revision": "5c39be77838e90fc8bbd",
+    "url": "/IlyaKirsanov/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a3c896aa0c813b5476a8",
+    "url": "/IlyaKirsanov/robofriends/static/css/main.3d85acfd.chunk.css"
+  },
+  {
+    "revision": "5c39be77838e90fc8bbd",
+    "url": "/IlyaKirsanov/robofriends/static/js/2.d09d550e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IlyaKirsanov/robofriends/static/js/2.d09d550e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3c896aa0c813b5476a8",
+    "url": "/IlyaKirsanov/robofriends/static/js/main.3efc7b89.chunk.js"
+  },
+  {
+    "revision": "27682e5adefdf21334dc",
+    "url": "/IlyaKirsanov/robofriends/static/js/runtime-main.5fe173ef.js"
+  }
+]);
